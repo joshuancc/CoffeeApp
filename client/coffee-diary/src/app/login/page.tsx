c1dc5e581faf0e9b97b1dd0bb1abf522ebe715a3
Calvin Tran <calvintran1478@protonmail.com>
2025-06-12 21:33:11 -0400

    Initialize client directory and add basic register and login pages

---
+++ client/coffee-diary/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1 className="text-gray-700 text-3xl">Login Page</h1>
+    </>
+  );
+}
---
+++ client/coffee-diary/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="text-gray-700 text-3xl">Landing Page</h1>
+    </>
+  );
+}

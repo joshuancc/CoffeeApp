e24756a3229ec9971f7b3e3dca3732a2860ab2e8
Calvin Tran <calvintran1478@protonmail.com>
2025-06-14 21:58:16 -0400

    Move table definitions to a separate sql file

--- server/coffee-diary/src/main/java/com/coffee/coffee_diary/CoffeeDiaryApplication.java
+++ server/coffee-diary/src/main/java/com/coffee/coffee_diary/CoffeeDiaryApplication.java
@@ -1,30 +1,13 @@
 package com.coffee.coffee_diary;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class CoffeeDiaryApplication implements CommandLineRunner {
+public class CoffeeDiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeDiaryApplication.class, args);
-	}
-
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-
-	@Override
-	public void run(String... strings) {
-		// Create user table
-		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS users(" +
-				"user_id UUID PRIMARY KEY," +
-				"email VARCHAR UNIQUE," +
-				"password VARCHAR NOT NULL" +
-		");"
-		);
 	}
 }
---
+++ server/coffee-diary/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS users(
+    user_id UUID PRIMARY KEY,
+    email VARCHAR UNIQUE,
+    password VARCHAR NOT NULL
+);

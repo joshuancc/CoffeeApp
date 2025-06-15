0cbec5c90382039d41e3381b51ab35870fa91ce3
Calvin Tran <calvintran1478@protonmail.com>
2025-06-14 20:28:59 -0400

    Add register and login functionality to the frontend

--- client/coffee-diary/src/app/login/page.tsx
+++ client/coffee-diary/src/app/login/page.tsx
@@ -1,8 +1,42 @@
+'use client'
+
+import { useState } from "react"; 
 
 export default function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    const response = await fetch("http://localhost:8080/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: `${email}\n${password}`
+    });
+
+    if (response.ok) {
+      alert("login success!");
+    } else {
+      alert(await response.text());
+    }
+  }
+
   return (
     <>
       <h1 className="text-gray-700 text-3xl">Login Page</h1>
+      <div className="flex flex-col">
+        <form action={loginUser}>
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" className="border w-64" onChange={(e) => setEmail(e.target.value)} required/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" className="border w-64" onChange={(e) => setPassword(e.target.value)} required/>
+          </div>
+          <button className="border mt-4">Login</button>
+        </form>
+      </div>
     </>
   );
 }
--- client/coffee-diary/src/app/register/page.tsx
+++ client/coffee-diary/src/app/register/page.tsx
@@ -1,8 +1,45 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useState } from "react"; 
 
 export default function RegisterPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const registerUser = async () => {
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: `${email}\n${password}`
+    });
+
+    if (response.ok) {
+      router.push("/login");
+    } else {
+      alert(await response.text());
+    }
+  }
+
   return (
     <>
       <h1 className="text-gray-700 text-3xl">Register Page</h1>
+      <div className="flex flex-col">
+        <form action={registerUser}>
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" className="border w-64" onChange={(e) => setEmail(e.target.value)} required/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" className="border w-64" onChange={(e) => setPassword(e.target.value)} required/>
+          </div>
+          <button className="border mt-4">Register</button>
+        </form>
+      </div>
     </>
   );
 }
